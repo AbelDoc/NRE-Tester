308f8dfa564fc6033011a679ae3bd0d953c86b23
Louis Abel <louis.abel5@etu.univ-lorraine.fr>
2019-06-18 00:03:56 +0200

    Initialize repository

---
+++ src/NRE_Main.cpp
@@ -0,0 +1,15 @@
+
+    /**
+     * @file NRE_Main.cpp
+     * @brief Test of Tester's API
+     * @author Louis ABEL
+     * @date 18/06/2019
+     * @copyright CC-BY-NC-SA
+     */
+
+    #include <iostream>
+
+    int main(int, char**) {
+
+        return 0;
+    }
